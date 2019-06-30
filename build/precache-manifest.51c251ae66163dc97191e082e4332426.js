55377b8891775673d3713527f00a88a7ef269727
Piotr Morawski <piotrmorawski90@gmail.com>
2019-06-30 12:59:19 +0200

    add build

---
+++ build/precache-manifest.51c251ae66163dc97191e082e4332426.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4043ba3a7fbac2bf0dbbadbb90cf9fe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49374426ca03b64c789d",
+    "url": "/static/css/main.b3040293.chunk.css"
+  },
+  {
+    "revision": "c79f52bf358a0e06412e",
+    "url": "/static/js/2.9a100f61.chunk.js"
+  },
+  {
+    "revision": "49374426ca03b64c789d",
+    "url": "/static/js/main.77e3ef2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a354edfd8f99eb3d56e70836b8251c2a",
+    "url": "/static/media/batman.a354edfd.png"
+  },
+  {
+    "revision": "d2705554e9bb6131480035a4ed2f0252",
+    "url": "/static/media/express.d2705554.png"
+  },
+  {
+    "revision": "17cb1ed2e8467b51bb26cf017daa9722",
+    "url": "/static/media/foundation-icons.17cb1ed2.svg"
+  },
+  {
+    "revision": "92827f088b9eda87169bdc2b9888ce52",
+    "url": "/static/media/foundation-icons.92827f08.eot"
+  },
+  {
+    "revision": "a188c2f768ce5033d3f5d47be7280e25",
+    "url": "/static/media/foundation-icons.a188c2f7.woff"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/static/media/foundation-icons.e20945d7.ttf"
+  },
+  {
+    "revision": "eba424ba3d4d534d2415cbc9328bdcf4",
+    "url": "/static/media/fruits.eba424ba.png"
+  },
+  {
+    "revision": "0b1a4ddfb909bbab86bc286404f83391",
+    "url": "/static/media/gameOfLife.0b1a4ddf.jpg"
+  },
+  {
+    "revision": "3747f88fa8a62c2d80194cf0fc7e5f55",
+    "url": "/static/media/github.3747f88f.svg"
+  },
+  {
+    "revision": "996e22cec03aba1c7f7d6abd0d9e762e",
+    "url": "/static/media/home.996e22ce.svg"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/static/media/linkedIn.5b238434.svg"
+  },
+  {
+    "revision": "d0941f3bfdbf42ac592928e0f2a7a195",
+    "url": "/static/media/parallax2.d0941f3b.jpg"
+  },
+  {
+    "revision": "1c2fb2e117f7e8fca3a1e706363b483b",
+    "url": "/static/media/photoID.1c2fb2e1.png"
+  },
+  {
+    "revision": "7ecd5f61a1158fed9a4312e313a9a61c",
+    "url": "/static/media/portfolio.7ecd5f61.jpg"
+  },
+  {
+    "revision": "3199c94501f2a5f5970dbf73da1611a0",
+    "url": "/static/media/prime.3199c945.png"
+  },
+  {
+    "revision": "604b840513a4c0a3ee5c74e2a0e9f7c0",
+    "url": "/static/media/www.604b8405.svg"
+  }
+]);
