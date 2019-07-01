1f5bd3e31a845f6f49d06cf27c79ddf02da8abc8
Piotr Morawski <piotrmorawski90@gmail.com>
2019-07-01 22:32:56 +0200

    fix responsive menu

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import AutoCarousel from "components/AutoCarousel/AutoCarousel"
 import Footer from "components/Footer/Footer"
-import InfoCard, { InfoLinkProps } from "components/InfoCard/InfoCard"
+import InfoCard from "components/InfoCard/InfoCard"
 import InfoCardSection from "components/InfoCardSection/InfoCardSection"
 import IntroSection from "components/IntroSection/IntroSection"
 import DownArrows from "components/MainParallax/DownArrows/DownArrows"
@@ -8,19 +8,6 @@ import ParallaxTitle from "components/MainParallax/ParallaxTitle/ParallaxTitle"
 import ProjectCardContainer from "components/ProjectCardContainer/ProjectCardContainer"
 import React from "react"
 import Sticky from "react-sticky-el"
-import { mongoDbIcon, nodeJSIcon } from "../../icons/backEndIcons"
-import { gitIcon, ubuntuIcon } from "../../icons/developmentIcons"
-import {
-  css3Icon,
-  html5Icon,
-  jQueryIcon,
-  JSIcon,
-  reactIcon,
-  SassIcon
-} from "../../icons/frontEndIcons"
-import { ReactComponent as GitHubSvg } from "../../icons/github.svg"
-import { ReactComponent as HomeSvg } from "../../icons/home.svg"
-import { ReactComponent as LinkedInSvg } from "../../icons/linkedIn.svg"
 import parallax from "../../photo/parallax3.jpg"
 import photoID from "../../photo/photoID.png"
 import IntroText from "../IntroText/IntroText"
@@ -28,35 +15,7 @@ import MainParallax from "../MainParallax/MainParallax"
 import Menu from "../Menu/Menu"
 import ProjectsSection from "../ProjectsSection/ProjectsSection"
 import "./App.scss"
-
-const frontEndIcons = [
-  reactIcon,
-  css3Icon,
-  html5Icon,
-  jQueryIcon,
-  JSIcon,
-  SassIcon
-]
-const backEndIcons = [mongoDbIcon, nodeJSIcon]
-const developmentIcons = [gitIcon, ubuntuIcon]
-
-const links: InfoLinkProps[] = [
-  {
-    address: "#",
-    description: "Portfolio",
-    icon: <HomeSvg />
-  },
-  {
-    address: "https://www.linkedin.com/in/piotrmorawski90/",
-    description: "LinkedIn",
-    icon: <LinkedInSvg />
-  },
-  {
-    address: "https://github.com/morawskioz",
-    description: "GitHub",
-    icon: <GitHubSvg />
-  }
-]
+import { frontEndIcons, backEndIcons, developmentIcons, links } from "./AppData"
 
 const App = () => {
   return (
@@ -82,16 +41,7 @@ const App = () => {
         <ProjectsSection id={"Projects"}>
           <ProjectCardContainer />
         </ProjectsSection>
-        <InfoCardSection id={"Info"}>
-          <InfoCard
-            city={"Wroclaw"}
-            email={"piotrmorawski90@gmail.com"}
-            fullName={"Piotr Morawski"}
-            title={"Front-End Developer"}
-            links={links}
-            photoUrl={photoID}
-          />
-        </InfoCardSection>
+
       </div>
       <Footer />
     </div>
